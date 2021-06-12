3c8cd39c36a727f01610f02ae8154c574a45ab46
leotis <mekdemcrossit@tutanota.com>
2021-06-12 07:49:15 -0400

    Feature: Building with pure functions If I were to boil functional programming down to just two rules, the first one would be this: prefer to write pure functions as often as you can

--- HackSwiftFucntionalProgramming/main.swift
+++ HackSwiftFucntionalProgramming/main.swift
@@ -1,11 +1,71 @@
 //
 //  main.swift
-//  HackSwiftFucntionalProgramming
+//  HackSwiftFunctionalProgramming
 //
 //  Created by Leotis buchanan on 2021-06-11.
 //
 
 import Foundation
 
-print("Hello, World!")
 
+struct Classroom {
+    let scores:[Double]
+    var normalize = false
+    var credits  = 10
+
+
+    mutating func chargeForCalculatingGrades(normalize:Bool = false) -> [String] {
+
+        if chargeUser() {
+            return calculateGrades(normalize: normalize)
+        } else {
+            return []
+        }
+    }
+
+    mutating func chargeUser() -> Bool {
+        guard credits >= 0 else {return false}
+        credits -= 1
+        return true
+    }
+
+
+    func calculateGrades(normalize:Bool = false) -> [String] {
+        var result = [String]()
+
+        if normalize {
+
+            let highest = scores.max() ?? 100
+            for score in scores {
+                if score > highest - 5 {
+
+                } else if score > highest - 15 {
+                    result.append("B")
+                } else if score > highest - 25 {
+                    result.append("C")
+                } else {
+                    result.append("Fail")
+                }
+            }
+        } else {
+            for score in scores {
+                if score > 95 {
+                    result.append("A")
+                } else if score > 80 {
+                    result.append("B")
+                } else if score > 70 {
+                    result.append("C")
+                } else {
+                    result.append("Fail")
+                }
+            }
+        }
+
+        return result
+
+    }
+
+}
+
+let classroom = Classroom(scores : [75,85,65,40])
+print(classroom.calculateGrades(normalize: true))
