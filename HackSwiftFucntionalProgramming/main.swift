ad39de98e1eea2e9e326a7892534b4b0e34cd59c
leotis <mekdemcrossit@tutanota.com>
2021-06-14 04:58:42 -0400

    feature:functions as first-class types
    If I were to boil functional programming down to just two rules, the second rule would be this: functions are first-class types, and should be passed around like any other kind of data. In this article we’re going to explore what that means, and what power it unleashes…

--- HackSwiftFucntionalProgramming/main.swift
+++ HackSwiftFucntionalProgramming/main.swift
@@ -1,71 +1,130 @@
+//////
+//////  main.swift
+//////  HackSwiftFunctionalProgramming
+//////
+//////  Created by Leotis buchanan on 2021-06-11.
+////// Functions as first class types
+//////
+////
+//import Foundation
+////
+////func meanAverage(_ numbers: [Double]) -> Double {
+////    guard numbers.isEmpty == false else {return 0}
+////
+////    var total = 0.0
+////    for number in numbers {
+////        total += number
+////
+////    }
+////
+////    return total/Double(numbers.count)
+////}
+////
+////let numbers:[Double] = [1,2,3,4,5]
+////let mean1 = meanAverage(numbers)
+////print(mean1)
+////let copiedFunction:([Double]) -> Double = meanAverage
+////let mean2 = copiedFunction(numbers)
+////print(mean2)
+////
+////struct Classroom {
+////    let teacher:String
+////    let averageColors: ([Double]) -> Double
+////}
+////
+//////you can pass in a predefine function
+////let archer = Classroom(teacher:"Mr Archer", averageColors: meanAverage)
+////
+////// or you can pass in a inline closure, like this
+////
+////let figgis = Classroom(teacher: "Mr Figgis") {
+////    numbers in
+////    guard numbers.isEmpty == false else {return 0}
+////
+////    var total = 0.0
+////    for number in numbers {
+////        total += number
+////
+////    }
+////
+////    return total/Double(numbers.count)
+////
+////}
+////
+//////you can also set a struct member variable to
+//////a function belonging to another struct like this
+////
+////struct User {
+////    let name = "Taylor Swift"
+////
+////    func printGreeting(){
+////        print("Hi, I'm \(name)!")
+////    }
+////}
+////
+////struct Employee {
+////    //greet team takes a function
+////    //that accepts no parameters and
+////    //returns voids
+////
+////    let greetTeam: () -> Void
+////}
+////
+////let taylor = User()
+//////here is where we assign greetTeam to
+//////taylor.printGreeting function
+////let cheryl = Employee(greetTeam: taylor.printGreeting)
+////
+////cheryl.greetTeam()
+////
+////
+////
+////
+////
 //
-//  main.swift
-//  HackSwiftFunctionalProgramming
-//
-//  Created by Leotis buchanan on 2021-06-11.
-//
-
-import Foundation
-
-
-struct Classroom {
-    let scores:[Double]
-    var normalize = false
-    var credits  = 10
-
-
-    mutating func chargeForCalculatingGrades(normalize:Bool = false) -> [String] {
-
-        if chargeUser() {
-            return calculateGrades(normalize: normalize)
-        } else {
-            return []
-        }
-    }
-
-    mutating func chargeUser() -> Bool {
-        guard credits >= 0 else {return false}
-        credits -= 1
-        return true
-    }
+////let not = (!)
+////
+////var loggedIn = false
+////
+////if not(loggedIn){
+////    print("You are anonymous")
+////}
+////
+////let add : (String,String) -> String = (+)
+////print(add("1","5"))
+////
+////let names = ["Bobby", "Jonatho", "Antoni", "Karamo", "Tan"]
+////// the greater > is a function, this can also be a custom design
+//////free function, or it can be a closure
+//////names.sorted{$0 > $1}
+//let sortedNames = names.sorted(by: >)
+////print(sortedNames)
 
 
-    func calculateGrades(normalize:Bool = false) -> [String] {
-        var result = [String]()
+let albums = ["Folklore", "Lover", "1989"]
+let result1 = albums.allSatisfy{$0.contains("o")}
+print(result1)
 
-        if normalize {
+let message = "Taylor Swift's most popular albums include Folklore, Lover and 1989."
+let result2 = message.contains("Lover")
+print(result2)
 
-            let highest = scores.max() ?? 100
-            for score in scores {
-                if score > highest - 5 {
+let result3 = albums.allSatisfy(message.contains)
+print(result3)
 
-                } else if score > highest - 15 {
-                    result.append("B")
-                } else if score > highest - 25 {
-                    result.append("C")
-                } else {
-                    result.append("Fail")
-                }
-            }
-        } else {
-            for score in scores {
-                if score > 95 {
-                    result.append("A")
-                } else if score > 80 {
-                    result.append("B")
-                } else if score > 70 {
-                    result.append("C")
-                } else {
-                    result.append("Fail")
-                }
-            }
-        }
+//closures are shared between the same function call
+//so the code below all use the same copy counter
 
-        return result
+var counter = 0
 
-    }
-
+let increment = {
+    counter += 1
+    print("Call count: \(counter)")
 }
 
-let classroom = Classroom(scores : [75,85,65,40])
-print(classroom.calculateGrades(normalize: true))
+let copy = increment
+
+increment()
+increment()
+copy()
+increment()
